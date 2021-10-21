aedf959fe810b18a09ac2e18d134f414082e113c
Tiago Seixas <tvseixas@gmail.com>
2021-10-21 19:29:28 -0300

    Separando Tx e Rx

--- Projeto 6/due_sw_uart_TX/due_sw_uart_TX.ino
+++ Projeto 6/due_sw_uart_TX/due_sw_uart_TX.ino
@@ -1,17 +1,18 @@
 #include "sw_uart.h"
 
-due_sw_uart uart_TX;
+due_sw_uart uart;
+char teste = 'a';
 
-void setup_TX() {
-  Serial.begin(9600);
+void setup() {
+  //Serial.begin(9600);
   sw_uart_setup(&uart, 4, 1, 8, SW_UART_EVEN_PARITY);
 }
 
-void loop_TX() {
+void loop() {
  test_write();
 }
 
 void test_write() {
-  sw_uart_write_string(&uart, "a");
+  sw_uart_write_string(&uart, teste);
   delay(1000);
 }
